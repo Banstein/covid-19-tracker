85e17c0c74047a6aad8c2054a85c98d93e1bd509
Banstein <islambahnas1@gmail.com>
2022-04-13 21:07:19 +0200

    Add all features

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getCountries } from './redux/countries/countries';
+import CountriesContainer from './components/CountriesContainer';
+import RegionsContainer from './components/RegionsContainer';
+import Navigation from './components/Navigation';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getCountries()(dispatch);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<CountriesContainer />} />
+        <Route path="/:id" element={<RegionsContainer />} />
+      </Routes>
     </div>
+
   );
 }
 
