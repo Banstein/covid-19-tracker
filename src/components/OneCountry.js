c8fa38a47c053edebbf738e2fd3d573635404de7
Banstein <islambahnas1@gmail.com>
2022-04-14 20:27:15 +0200

    Update styles

--- src/components/OneCountry.js
+++ src/components/OneCountry.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import map from '../assets/images/icons8-map-160.png';
 import './OneCountry.css';
 
 function OneCountry({ name, today_confirmed }) {
@@ -10,7 +9,7 @@ function OneCountry({ name, today_confirmed }) {
     <NavLink to={`/${name}`}>
       <li className="card">
         <div>
-          <img className="map" src={map} alt="map" />
+          <img className="map" src="https://raw.githubusercontent.com/djaiss/mapsicon/33ba28808f8d32b5bae0ffada9cadd07073852e1/all/ad/vector.svg" alt="map" />
           <i className="fa-regular fa-circle-right" />
         </div>
         <h5>{name}</h5>
